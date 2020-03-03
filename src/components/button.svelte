dae19244d5abe4894370e485607859ee34431939
newbornfrontender <newbornfrontender@gmail.com>
2020-03-03 17:20:34 +0300

    feat: add base app

---
+++ src/components/button.svelte
@@ -0,0 +1,13 @@
+<script context="module">
+  import { createStore, forward } from 'effector';
+  import { value } from './field';
+
+  const disabled = createStore(true);
+
+  forward({
+    from: value.map((state) => state.length === 0),
+    to: disabled,
+  });
+</script>
+
+<button type="submit" disabled={$disabled}>Check</button>
